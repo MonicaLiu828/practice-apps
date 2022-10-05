b861db3166947e164e920ffd051e8f639fda147e
Monica Liu <liuqianmonica@gmail.com>
2022-10-05 16:17:43 -0700

    almost complete back-end

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,57 @@
 const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/glossary');
 
-// 1. Use mongoose to establish a connection to MongoDB
-// 2. Set up any schema and models needed by the app
-// 3. Export the models
-// 4. Import the models into any modules that need them
+
+const glossarySchema = new mongoose.Schema({
+ wordname: {
+  type: String,
+  unique: true
+ },
+ definition: String
+});
+
+const GloModel = mongoose.model('glossaryList', glossarySchema);
+
+
+// const laptop = new GloModel({ wordname: 'laptop', definition: 'a small computer you can use on your lap'});
+// const desktop = new GloModel({ wordname: 'desktop', definition: 'a large computer'});
+
+const listSave = function (data) {
+  const laptop = new GloModel({ wordname: 'laptop', definition: 'a small computer you can use on your lap'});
+  const desktop = new GloModel({ wordname: 'desktop', definition: 'a large computer'});
+
+  // var res = [laptop.save(), desktop.save()];
+  laptop.save().catch((err) => {
+    console.log(err);
+  })
+  desktop.save().catch((err) => {
+    console.log(err);
+  })
+  var res = [];
+  if (data !== undefined) {
+    const eachData = new GloModel(data);
+    res.push(eachData.save());
+  }
+  return Promise.all(res)
+}
+
+const listGet = function () {
+  return GloModel.find().exec()
+}
+
+// laptop.save((err) => {
+//   if (err) {
+//     console.log(err)
+//   }
+
+// })
+// desktop.save((err) => {
+//   if (err) {
+//     console.log(err);
+//   }
+// })
+
+module.exports.GloModel = GloModel;
+module.exports.listSave = listSave;
+module.exports.listGet = listGet;
+
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,52 @@
+const db = require('./db.js');
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
 const app = express();
-
+app.use(express.json())
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+// Other routes here....
+app.get('/glossaryList', (req, res) => {
+
+  // const laptop = new db.GloModel({ wordname: 'laptop', definition: 'a small computer you can use on your lap'});
+  // const desktop = new db.GloModel({ wordname: 'desktop', definition: 'a large computer'});
+  // laptop.save((err) => {
+  //   if (err) return handleError(err);
+  // })
+  // desktop.save((err) => {
+  //   if (err) return handleError(err);
+  // })
+  db.listSave()
+  .then(() => {
+    console.log('save succeed')
+     db.listGet()
+     .then((data) => {
+      res.send(JSON.stringify(data))
+     })
+  })
+  .catch((err) => {
+    console.log(err);
+    res.end();
+  })
+})
+
+// app.post()
+app.post('/glossaryList', (req, res) => {
+  var eachglossary = req.body;
+  db.listSave(eachglossary)
+  .then((eachglossary) => {
+    console.log('your glossary saved!!')
+    res.send('your glossary saved')
+  })
+  .catch((err) => {
+    res.send(err);
+  })
+})
+
+// app.put()
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
